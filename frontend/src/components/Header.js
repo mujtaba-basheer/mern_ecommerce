33a68df98b9bcc48543eb5d34d9e5e758e32c63e
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2023-05-22 22:01:13 +0530

    ...

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,88 +7,77 @@ import SearchBox from "./SearchBox";
 import { logout } from "../actions/userActions";
 
 const Header = () => {
-    const [name, setName] = useState("");
+  const [name, setName] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const { userInfo } = useSelector((state) => state.userLogin);
+  const { userInfo } = useSelector((state) => state.userLogin);
 
-    const { user } = useSelector((state) => state.userDetails);
+  const { user } = useSelector((state) => state.userDetails);
 
-    useEffect(() => {
-        if (userInfo) {
-            if (user.name) setName(user.name);
-            else setName(userInfo.name);
-        }
-    }, [setName, user, userInfo]);
+  useEffect(() => {
+    if (userInfo) {
+      if (user.name) setName(user.name);
+      else setName(userInfo.name);
+    }
+  }, [setName, user, userInfo]);
 
-    const logoutHandler = () => dispatch(logout());
+  const logoutHandler = () => dispatch(logout());
 
-    return (
-        <header>
-            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-                <Container>
-                    <LinkContainer to="/">
-                        <Navbar.Brand>ProShop</Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Route
-                            render={({ history }) => (
-                                <SearchBox history={history} />
-                            )}
-                        />
-                        <Nav className="ml-auto">
-                            <LinkContainer to="/cart">
-                                <Nav.Link>
-                                    <i className="fas fa-shopping-cart"></i>
-                                    Cart
-                                </Nav.Link>
-                            </LinkContainer>
-                            {userInfo ? (
-                                <NavDropdown title={name} id="username">
-                                    <LinkContainer to="/profile">
-                                        <NavDropdown.Item>
-                                            Profile
-                                        </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <NavDropdown.Item onClick={logoutHandler}>
-                                        Logout
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            ) : (
-                                <LinkContainer to="/login">
-                                    <Nav.Link>
-                                        <i className="fas fa-user"></i>
-                                        Sign In
-                                    </Nav.Link>
-                                </LinkContainer>
-                            )}
-                            {userInfo && userInfo.isAdmin && (
-                                <NavDropdown title="Admin" id="adminmenu">
-                                    <LinkContainer to="/admin/userlist">
-                                        <NavDropdown.Item>
-                                            Users
-                                        </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <LinkContainer to="/admin/productlist">
-                                        <NavDropdown.Item>
-                                            Products
-                                        </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <LinkContainer to="/admin/orderlist">
-                                        <NavDropdown.Item>
-                                            Orders
-                                        </NavDropdown.Item>
-                                    </LinkContainer>
-                                </NavDropdown>
-                            )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    );
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            ``
+            <Route render={({ history }) => <SearchBox history={history} />} />
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i>
+                    Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
